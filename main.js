8eae80023f61875cc8f2d582cc3a6829b58302db
dhananjayan573 <dhananjayan573@gmail.com>
2021-01-11 20:21:34 +0530

    inital commit

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).on('click', 'ul li', function () {
+    $(this).addClass('active').siblings().removeClass('active')
+})
+
+
+$(document).ready(function () {
+    $('#icon').click(function () {
+        $('ul').toggleClass('show');
+    });
+});
+
+
+$(document).ready(function () {
+    $('ul li').click(function () {
+        $('ul').toggleClass('show');
+    });
+});
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwR7opDxOi2UvLPa-BZ4L7M6cDJT70N4GBwcpnF9O0ZdIxOWcg/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    document.getElementById('thank').classList.add('show')
+})
+
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', e => {
+    const isChecked = e.target.checked;
+    if (isChecked) {
+        document.documentElement.className = 'theme-dark';
+    } else {
+        document.documentElement.className = 'theme-light';
+    }
+});
+
